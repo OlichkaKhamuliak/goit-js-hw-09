92db5a5bd44995a855a591f298c89a2222eee95e
OlichkaKhamuliak <olkoboyko97@gmail.com>
2023-11-27 11:57:21 +0200

    done task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,9 +5,9 @@ const delayStepEl = document.querySelector('input[name=step]')
 const amountEl = document.querySelector('input[name=amount]')
 const btnEl = document.querySelector('button')
 
-btnEl.addEventListener('submit', handleSubmit)
+btnEl.addEventListener('click', handleClickCreate)
 
-function createPromises(position, delay) {
+function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
 
     setTimeout(() => {
@@ -21,14 +21,14 @@ function createPromises(position, delay) {
   });
 }
 
-function handleSubmit(evt) {
+function handleClickCreate(evt) {
   evt.preventDefault();
-  let firstDelay = Number(firstDelayEl.value);
-  let delayStep = Number(delayStepEl.value);
-  let amount = Number(amountEl.value);
+  const  firstDelay = Number(firstDelayEl.value);
+  const  delayStep = Number(delayStepEl.value);
+  const  amount = Number(amountEl.value);
 
     for (let i = 0; i < amount; i += 1) {
-      createPromises(i + 1, firstDelay + i * delayStep)
+      createPromise(i + 1, firstDelay + i * delayStep)
         .then(({ position, delay }) => {
           Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
         })
